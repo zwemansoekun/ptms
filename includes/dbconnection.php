a73a9871154818c285120e1d37c1bc708063dd5b
zwemansoekun <45864942+zwemansoekun@users.noreply.github.com>
2024-07-29 22:56:13 +0630

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "ptmsdb");
+$con=mysqli_connect("iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306", "ee2x6p69597fkt6g", "ptf2tzmw7ikzy4js", "h6gtxvb80mc4k5dk");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
